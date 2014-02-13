cebbfd208afecf227b3c74e0db44ca10374fec06
Andrew Monks <a@monks.co>
2014-02-13 05:52:55 -0600

    tests

--- doger_test.js
+++ doger_test.js
@@ -7,49 +7,30 @@ var expect = chai.expect;
 describe("doger.js", function() {
 
 
-	describe("Make doge image.", function() {
-		var image = "http://static.ddmcdn.com/en-us/apl/breedselector/images/breed-selector/dogs/breeds/shiba-inu_01_lg.jpg";
-		var doge_text = [ "much test.", "very units.", "much behavioral.", "amaze."]
-		var output = make_doge_image(image, doge_text);
-
-		it("Should return an object.", function() {
-			expect(output).to.be.a('object');	
+	describe("Get the current query string. (One of these will always fail)", function() {
+		var currentQueryString = window.location.href.slice(window.location.href.indexOf('?') + 1)
+		output = get_query_string();
+		it("Should return null if there is no query string", function() {
+			expect(output).to.be.null;
 		});
-
-		it("Should contain a div classed .doger", function() {
-			expect($('<div>').append(output.clone()).html()).to.have.string('<div class="doger">')
-			expect($('<div>').append(output.clone()).html()).to.have.string('</div>')
-		})
-
-		it("Should contain an image within the div.", function() {
-			expect(output.html()).to.have.string('<img');	
-		});
-
-		it("Should contain the input text.", function() {
-			for (var i = doge_text.length - 1; i >= 0; i--) {
-				expect(output.html()).to.have.string(doge_text[i]);	
-			};
+		it("Should return a string if there is a query string", function() {
+			expect(output).to.be.a('string');
 		});
 	});
 
 
-	describe("Find the largest image on a page.", function() {
-		var url = "http://www.popsci.com/scitech/article/2009-09/squirt-stem-cell-gel-heals-brain-injuries";
-		var output = image_from(url);
-
-		// it("Should return a url.", function() {
-		// 	// absurdly overcomplicated regex to too-liberally match urls; from https://gist.github.com/gruber/8891611
-		// 	regex = /\b((?:https?:(?:\/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b\/?(?!@))/i;
-		// 	expect(output).to.match(regex);
-		// });
-
-		it("Should return a string.", function() {
-			expect(output).to.be.a('string');	
+	describe("Check whether something is a url", function() {
+		var fakeURLs = [ "hello", "www.example", "http://wwwcom.netmail/", "guacamole/index.html"];
+		var realURLs = [ "http://google.com/", "http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html?bageldonut=slash&otherstuff", "monks.co", "arduino.cc/doc" ];
+		it("should return false for nonurl strings", function() {
+			for (var i = fakeURLs.length - 1; i >= 0; i--) {
+				expect(check_for_url( fakeURLs[i] )).to.be.false;
+			};
 		});
-
-		it("Should return the largest image on a page.", function() {
-			var correctValue = "http://www.popsci.com/sites/popsci.com/files/styles/article_image_large/public/images/2009/09/Brain_injury_with_herniation_MRI.jpg?itok=Sq_deapY";
-			expect(output).to.equal(correctValue);	
+		it("should return true for urls", function() {
+			for (var i = realURLs.length - 1; i >= 0; i--) {
+				expect(check_for_url( realURLs[i] )).to.be.true;
+			};
 		});
 	});
 
@@ -135,30 +116,50 @@ describe("doger.js", function() {
 	});
 
 
-	describe("Get the current query string. (One of these will always fail)", function() {
-		var currentQueryString = window.location.href.slice(window.location.href.indexOf('?') + 1)
-		output = get_query_string();
-		it("Should return null if there is no query string", function() {
-			expect(output).to.be.null;
+	describe("Find the largest image on a page.", function() {
+		var url = "http://www.popsci.com/scitech/article/2009-09/squirt-stem-cell-gel-heals-brain-injuries";
+		var output = image_from(url);
+
+		// it("Should return a url.", function() {
+		// 	// absurdly overcomplicated regex to too-liberally match urls; from https://gist.github.com/gruber/8891611
+		// 	regex = /\b((?:https?:(?:\/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b\/?(?!@))/i;
+		// 	expect(output).to.match(regex);
+		// });
+
+		it("Should return a string.", function() {
+			expect(output).to.be.a('string');	
 		});
-		it("Should return a string if there is a query string", function() {
-			expect(output).to.be.a('string');
+
+		it("Should return the largest image on a page.", function() {
+			var correctValue = "http://www.popsci.com/sites/popsci.com/files/styles/article_image_large/public/images/2009/09/Brain_injury_with_herniation_MRI.jpg?itok=Sq_deapY";
+			expect(output).to.equal(correctValue);	
 		});
 	});
 
 
-	describe("Check whether something is a url", function() {
-		var fakeURLs = [ "hello", "www.example", "http://wwwcom.netmail/", "guacamole/index.html"];
-		var realURLs = [ "http://google.com/", "http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html?bageldonut=slash&otherstuff", "monks.co", "arduino.cc/doc" ];
-		it("should return false for nonurl strings", function() {
-			for (var i = fakeURLs.length - 1; i >= 0; i--) {
-				expect(check_for_url( fakeURLs[i] )).to.be.false;
-			};
+	describe("Overlay doge keywords onto image.", function() {
+		var image = "http://static.ddmcdn.com/en-us/apl/breedselector/images/breed-selector/dogs/breeds/shiba-inu_01_lg.jpg";
+		var doge_text = [ "much test.", "very units.", "much behavioral.", "amaze."]
+		var output = make_doge_image(image, doge_text);
+
+		it("Should return an object.", function() {
+			expect(output).to.be.a('object');	
 		});
-		it("should return true for urls", function() {
-			for (var i = realURLs.length - 1; i >= 0; i--) {
-				expect(check_for_url( realURLs[i] )).to.be.true;
+
+		it("Should contain a div classed .doger", function() {
+			expect($('<div>').append(output.clone()).html()).to.have.string('<div class="doger">')
+			expect($('<div>').append(output.clone()).html()).to.have.string('</div>')
+		})
+
+		it("Should contain an image within the div.", function() {
+			expect(output.html()).to.have.string('<img');	
+		});
+
+		it("Should contain the input text.", function() {
+			for (var i = doge_text.length - 1; i >= 0; i--) {
+				expect(output.html()).to.have.string(doge_text[i]);	
 			};
 		});
 	});
+
 });
