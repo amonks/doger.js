ae6d93a4eee3476caf26d94701ebfc1fa2073802
Andrew Monks <a@monks.co>
2014-02-15 00:58:42 -0600

    check_url

--- doger_test.js
+++ doger_test.js
@@ -185,7 +185,7 @@ describe("doger.js", function() {
 
 		describe("Check whether something is a url", function() {
 			var fakeURLs = [ "hello.com", "http://wwwcom.netmail/", "guacamole/index.html"];
-			var realURLs = [ "http://google.com/", "http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html?bageldonut=slash&otherstuff", "monks.co", "arduino.cc/doc" ];
+			var realURLs = [ "http://google.com/", "http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html?bageldonut=slash&otherstuff", "http://monks.co", "http://arduino.cc/doc" ];
 			it("should return false for nonurl strings", function() {
 				for (var i = fakeURLs.length - 1; i >= 0; i--) {
 					expect(Doger.check_for_url( fakeURLs[i] )).to.be.false;
