d1fdbd3fbe09cc2a92a68576e5468cf9c4d91416
Andrew Monks <a@monks.co>
2014-02-28 01:34:16 -0600

    added remove_stopwords method tests

--- js/doger_test.js
+++ js/doger_test.js
@@ -329,11 +329,11 @@ describe("doger.js", function() {
 
 
 		describe("Find the union of two arrays", function() {
-			var array1 = [1, 2, 3];
-			var array2 = [101, 2, 1, 10];
-			var expectedOut = [1, 2, 3, 102, 10];
-			var out = Doger.union_arrays(array1, array2);
 			it("should return the union of two arrays", function() {
+				var array1 = [1, 2, 3];
+				var array2 = [101, 2, 1, 10];
+				var expectedOut = [1, 2, 3, 102, 10];
+				var out = Doger.union_arrays(array1, array2);
 				expect(out.length).to.equal(expectedOut.length);
 				for (var i = expectedOut.length - 1; i >= 0; i--) {
 					expect(out.indexOf(expectedOut[i])).to.be.above(-1);
@@ -343,12 +343,12 @@ describe("doger.js", function() {
 
 
 		describe("Find the intersection of two arrays", function() {
-			var array1 = [1, 2, 3];
-			var array2 = [101, 2, 1, 10];
-			var expectedOut = [1, 2];
-			var out = Doger.intersection_arrays(array1, array2);
-			expect(out.length).to.equal(expectedOut.length);
 				it("should return the intersection of two arrays", function() {
+				var array1 = [1, 2, 3];
+				var array2 = [101, 2, 1, 10];
+				var expectedOut = [1, 2];
+				var out = Doger.intersection_arrays(array1, array2);
+				expect(out.length).to.equal(expectedOut.length);
 				for (var i = expectedOut.length - 1; i >= 0; i--) {
 					expect(out.indexOf(expectedOut[i])).to.be.above(-1);
 				};
@@ -357,11 +357,11 @@ describe("doger.js", function() {
 
 
 		describe("Find the difference of two arrays", function() {
-			var array1 = [1, 2, 3, 4, 5];
-			var array2 = [5, 2, 10];
-			var expectedOut = [1, 3, 4];
-			var out = Doger.difference_arrays(array1, array2);
 			it("should return the difference of two arrays", function() {
+				var array1 = [1, 2, 3, 4, 5];
+				var array2 = [5, 2, 10];
+				var expectedOut = [1, 3, 4];
+				var out = Doger.difference_arrays(array1, array2);
 				expect(out.length).to.equal(expectedOut.length);
 				for (var i = expectedOut.length - 1; i >= 0; i--) {
 					expect(out.indexOf(expectedOut[i])).to.be.above(-1);
