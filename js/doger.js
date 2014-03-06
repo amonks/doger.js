2796024552361e56ceeacf2d216eb0416412c02f
Andrew Monks <a@monks.co>
2014-03-06 12:11:50 -0600

    remove stopwords from yahoo output

--- js/doger.js
+++ js/doger.js
@@ -233,15 +233,16 @@ Doger = {
             var notStopWords = Doger.remove_stopwords_from_array( text.replace(/[^a-zA-Z\d\s:]/, '').split(" ") );
             var popular = Doger.sort_array_by_frequency(notStopWords).slice(0, 4);
             output = output.concat(popular)
+            output = Doger.remove_stopwords_from_array(output, Doger.stopwords);
         };
         if (output !== undefined) {
             if (output.length <= 2) {
                 var notStopWords = Doger.remove_stopwords_from_array( text.replace(/[^a-zA-Z\d\s:]/, '').split(" ") );
                 var popular = Doger.sort_array_by_frequency(notStopWords).slice(0, 4);
                 output = output.concat(popular)
+                output = Doger.remove_stopwords_from_array(output, Doger.stopwords);
             };
         };
-        output = Doger.remove_stopwords_from_array(output, Doger.stopwords);
         return output;
     },
 
